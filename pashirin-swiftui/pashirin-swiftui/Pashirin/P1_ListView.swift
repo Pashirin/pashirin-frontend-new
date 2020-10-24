5ade1fa430524ba8819b3025046e271b15fa6b36
ryutamura <rtamura@bluecatnetworks.com>
2020-10-24 16:54:55 +0900

    Deleted tabview and changed the destination of navigation link to P3_Interim

--- pashirin-swiftui/pashirin-swiftui/Pashirin/P1_ListView.swift
+++ pashirin-swiftui/pashirin-swiftui/Pashirin/P1_ListView.swift
@@ -14,80 +14,37 @@ struct P1_ListView: View {
     //後で消す
     
     var body: some View {
-       // NavigationView{
-        TabView{
             List(viewModel.contacts) { contact in
-               
-//                HStack{
-//                    Image("profile")
-//                        .resizable()
-//                        .frame(width: 60, height: 60)
-//                        .clipped()
-//                        .cornerRadius(50)
-//                    }
-            NavigationLink(destination:P3_DetailView (contact: contact)){
                 
-                VStack(alignment: .leading){
-                    Text(contact.name)
-                        .font(.system(size: 21, weight: .medium, design: .default))
-                    Text("Offer Price: ¥\(contact.price)")
-                        .font(.system(size: 21, weight: .medium, design: .default))
-                    Text("Rating: \(contact.rating)")
-                        .font(.subheadline)
+                //                HStack{
+                //                    Image("profile")
+                //                        .resizable()
+                //                        .frame(width: 60, height: 60)
+                //                        .clipped()
+                //                        .cornerRadius(50)
+                //                    }
+                NavigationLink(destination:P3_Interim (contact: contact)){
+                    
+                    VStack(alignment: .leading){
+                        Text(contact.name)
+                            .font(.system(size: 21, weight: .medium, design: .default))
+                        Text("Offer Price: ¥\(contact.price)")
+                            .font(.system(size: 21, weight: .medium, design: .default))
+                        Text("Rating: \(contact.rating)")
+                            .font(.subheadline)
+                    }
                 }
+                .navigationBarTitle("PashiList")
+                .navigationBarBackButtonHidden(true)
+
             }
-            .navigationBarBackButtonHidden(true)
-          }
-            .navigationBarTitle("PashiList")
+
             .onAppear(){
                 self.viewModel.fetchData()
                 print("onApearに入りました")
                 UserDefaults.standard.set("67rCkmZExMNqquAJdCDShUfGF5f1", forKey: "current_user_id")
                 print(UserDefaults.standard.string(forKey: "current_user_id")!)
-                }
-            
-            .tabItem {
-                Image(systemName: "magnifyingglass")
-    
-                    .font(.system(size: 40))
-                    .foregroundColor(Color.gray)
-                Text("Request")
-                    .font(.system(size: 15))
-                    .foregroundColor(Color.gray)
             }
-        
-        
-            PashirinHistory()
-                .tabItem{
-                Image(systemName: "clock")
-                
-                    .font(.system(size: 40))
-                    .foregroundColor(Color.gray)
-                    
-                Text("History")
-                    .font(.system(size: 15))
-                    .foregroundColor(Color.gray)
-            }
-            
-            AccountDetail()
-                .tabItem{
-                Image(systemName: "person.fill")
-                    
-                    .font(.system(size: 40))
-                    .foregroundColor(Color.gray)
-                
-                Text("Account")
-                    .font(.system(size: 15))
-                    .foregroundColor(Color.gray)
-                }
-        }
-        .font(.headline)
-        // chage color when tap button
-        .accentColor(Color(red: 254/255, green: 139/255, blue: 93/255))
-        // make background white
-        .onAppear() {
-            UITabBar.appearance().barTintColor = .white
-        }
     }
 }
 
