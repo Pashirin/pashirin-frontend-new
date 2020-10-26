89373c04443e180705d2a35d4fa05d17dd62be27
ryutamura <rtamura@bluecatnetworks.com>
2020-10-26 13:54:41 +0900

    modified

--- pashirin-swiftui/pashirin-swiftui/Pashirin/P1_ListView.swift
+++ pashirin-swiftui/pashirin-swiftui/Pashirin/P1_ListView.swift
@@ -18,38 +18,38 @@ struct P1_ListView: View {
     //後で消す
     
     var body: some View {
-            List(viewModel.contacts) { contact in
+        List(viewModel.contacts) { contact in
+            
+            //                HStack{
+            //                    Image("profile")
+            //                        .resizable()
+            //                        .frame(width: 60, height: 60)
+            //                        .clipped()
+            //                        .cornerRadius(50)
+            //                    }
+            NavigationLink(destination:P3_Interim (contact: contact).navigationBarBackButtonHidden(true)){
                 
-                //                HStack{
-                //                    Image("profile")
-                //                        .resizable()
-                //                        .frame(width: 60, height: 60)
-                //                        .clipped()
-                //                        .cornerRadius(50)
-                //                    }
-                NavigationLink(destination:P3_Interim (contact: contact).navigationBarBackButtonHidden(true)){
-                    
-                    VStack(alignment: .leading){
-                        Text(contact.name)
-                            .font(.system(size: 21, weight: .medium, design: .default))
-                        Text("Offer Price: ¥\(contact.price)")
-                            .font(.system(size: 21, weight: .medium, design: .default))
-                        Text("Rating: \(contact.rating)")
-                            .font(.subheadline)
-                    }
+                VStack(alignment: .leading){
+                    Text(contact.name)
+                        .font(.system(size: 21, weight: .medium, design: .default))
+                    Text("Offer Price: ¥\(contact.price)")
+                        .font(.system(size: 21, weight: .medium, design: .default))
+                    Text("Rating: \(contact.rating)")
+                        .font(.subheadline)
                 }
-                .navigationBarTitle("PashiList")
-                .navigationBarBackButtonHidden(true)
-
             }
-
-            .onAppear(){
-                self.viewModel.fetchData()
-                print("onAppearに入りました")
-                getUserId()
-                UserDefaults.standard.set(self.userId, forKey: "current_user_id")
-                print(UserDefaults.standard.string(forKey: "current_user_id")!)
-            }
+            .navigationBarTitle("PashiList")
+            .navigationBarBackButtonHidden(true)
+            
+        }
+        
+        .onAppear(){
+            self.viewModel.fetchData()
+            print("onAppearに入りました")
+            getUserId()
+            UserDefaults.standard.set(self.userId, forKey: "current_user_id")
+            print(UserDefaults.standard.string(forKey: "current_user_id")!)
+        }
     }
     
     func getUserId() {
